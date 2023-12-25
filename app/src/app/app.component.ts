cdfbf98490c58ecaf8d9aa8119a421f92d5d3807
João Otávio <joaotavioos@outlook.com>
2023-12-25 13:00:30 -0300

    feat: update imports ✨

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { LayoutComponent } from "./layout/layout.component";
+import { LayoutComponent } from './layout/layout.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, LayoutComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [CommonModule, RouterOutlet, LayoutComponent],
 })
 export class AppComponent {
   title = 'app';
--- app/src/app/layout/layout.component.ts
+++ app/src/app/layout/layout.component.ts
@@ -1,16 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "../header/header.component";
+import { HeaderComponent } from '../header/header.component';
+import { BannerComponent } from '../banner/banner.component';
 
 @Component({
-    selector: 'app-layout',
-    standalone: true,
-    templateUrl: './layout.component.html',
-    styleUrl: './layout.component.css',
-    imports: [CommonModule, RouterOutlet, HeaderComponent]
+  selector: 'app-layout',
+  standalone: true,
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.css',
+  imports: [CommonModule, RouterOutlet, HeaderComponent, BannerComponent],
 })
 export class LayoutComponent {
   title = 'app';
-
 }
