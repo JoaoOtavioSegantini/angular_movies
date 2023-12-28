e4267016c1f237b6e724addf8fe357f6fc49df42
João Otávio <joaotavioos@outlook.com>
2023-12-28 09:34:42 -0300

    feat: add movie-info component ✨

---
+++ app/src/app/movie-info/movie-info.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input } from '@angular/core';
+import { Movie } from '../../typings/movie';
+import { CommonModule } from '@angular/common';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import {
+  heroUserGroup,
+  heroSpeakerWave,
+  heroChevronDown,
+  heroPlay,
+  heroPlus,
+} from '@ng-icons/heroicons/outline';
+import { MovieRatingComponent } from '../movie-rating/movie-rating.component';
+
+@Component({
+  selector: 'app-movie-info',
+  standalone: true,
+  templateUrl: './movie-info.component.html',
+  styleUrl: './movie-info.component.css',
+  providers: [
+    provideIcons({
+      heroUserGroup,
+      heroPlay,
+      heroChevronDown,
+      heroSpeakerWave,
+      heroPlus,
+    }),
+  ],
+  imports: [CommonModule, NgIconComponent, MovieRatingComponent],
+})
+export class MovieInfoComponent {
+  @Input({ required: true })
+  movie: Movie | undefined;
+}
