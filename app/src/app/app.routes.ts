4c1ab6dddd5759c8decfee8eac3af469dffeffa1
João Otávio <joaotavioos@outlook.com>
2023-12-28 09:14:05 -0300

    feat: add app routes ✨

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { SearchComponent } from './page/search/search.component';
+import { LayoutComponent } from './layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+  },
+  { path: 'search', component: SearchComponent },
+];
