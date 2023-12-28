966b575e6dce4569e7e6c649e21f946456fec9e6
João Otávio <joaotavioos@outlook.com>
2023-12-28 09:32:25 -0300

    feat: add search navigation in header component ✨

--- app/src/app/header/header.component.spec.ts
+++ app/src/app/header/header.component.spec.ts
@@ -8,10 +8,9 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent]
-    })
-    .compileComponents();
-    
+      imports: [HeaderComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +18,23 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should respond to scroll events', async () => {
+    // simulate a scroll event
+    expect(component.isScrolled).toBeFalsy();
+
+    // simulate a scroll event
+    window.scrollY = 50;
+    window.dispatchEvent(new Event('scroll'));
+
+    // assert variable value
+    expect(component.isScrolled).toBeTruthy();
+
+    // simulate a scroll event to top
+    window.scrollY = 0;
+    window.dispatchEvent(new Event('scroll'));
+
+    // assert variable value
+    expect(component.isScrolled).toBeFalsy();
   });
 });
--- app/src/app/header/header.component.ts
+++ app/src/app/header/header.component.ts
@@ -1,16 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { Component, HostListener } from '@angular/core';
-import { Scroll } from '@angular/router';
+import {
+  ActivatedRoute,
+  Router,
+  RouterLink,
+  RouterOutlet,
+  Scroll,
+} from '@angular/router';
+import { SearchFormComponent } from '../search-form/search-form.component';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
+  imports: [CommonModule, SearchFormComponent, RouterOutlet, RouterLink],
 })
 export class HeaderComponent {
   isScrolled = false;
+  searchTerm: string | undefined;
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   @HostListener('window:scroll', ['$event.target']) // for scroll events of the current element
   handleScroll(event: Scroll) {
@@ -23,4 +34,13 @@ export class HeaderComponent {
   resetScroll() {
     this.isScrolled = false;
   }
+
+  onSearch = (f: NgForm) => {
+    this.router.navigate([`/search`], {
+      queryParams: {
+        title: f.form.value.search,
+      },
+      queryParamsHandling: 'merge',
+    });
+  };
 }
