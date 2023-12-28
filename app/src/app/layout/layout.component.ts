24b611511d9b548638ddf9c637adf804f41bf014
João Otávio <joaotavioos@outlook.com>
2023-12-28 09:33:29 -0300

    feat: add movies programmatically ✨

--- app/src/app/layout/layout.component.ts
+++ app/src/app/layout/layout.component.ts
@@ -1,16 +1,43 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
 import { BannerComponent } from '../banner/banner.component';
+import { MovieRowComponent } from '../movie-row/movie-row.component';
+import { MovieServiceService } from '../service/movie-service.service';
+import { IMovies, Movie } from '../../typings/movie';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
-  imports: [CommonModule, RouterOutlet, HeaderComponent, BannerComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    BannerComponent,
+    MovieRowComponent,
+  ],
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
   title = 'app';
+  service = new MovieServiceService();
+  featuredMovie: Movie | undefined;
+  movies!: IMovies;
+
+  async ngOnInit(): Promise<void> {
+    this.featuredMovie = await this.service.getFeaturedMovie('106');
+    const genres = ['Drama', 'Action', 'Comedy', 'Animation'];
+
+    this.movies = await Promise.all(
+      genres.map(async (genre) => {
+        const movies = await this.service.getMoviesByGenre(genre, {
+          _limit: 8,
+        });
+        return { sectionTitle: genre, movies };
+      })
+    );
+  }
 }
+
