30f4f6e24b1dd8c7b5685eaff9085c382a2e9372
João Otávio <joaotavioos@outlook.com>
2023-12-29 17:55:53 -0300

    test: add movie-rating tests 🚨

--- app/src/app/movie-rating/movie-rating.component.spec.ts
+++ app/src/app/movie-rating/movie-rating.component.spec.ts
@@ -8,10 +8,9 @@ describe('MovieRatingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MovieRatingComponent]
-    })
-    .compileComponents();
-    
+      imports: [MovieRatingComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(MovieRatingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +18,73 @@ describe('MovieRatingComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('renders without crashing', () => {
+    const fixture = TestBed.createComponent(MovieRatingComponent);
+    fixture.componentInstance.rating = 'pg';
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    fixture.detectChanges();
+    expect(compiled.querySelector('span')?.textContent).toContain('pg');
+  });
+
+  it('displays the correct rating', () => {
+    const fixture = TestBed.createComponent(MovieRatingComponent);
+    fixture.componentInstance.rating = 'pg-13';
+
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    fixture.detectChanges();
+    expect(compiled.querySelector('span')?.textContent).toContain('pg-13');
+  });
+
+  it('applies the correct text color for rating "pg"', () => {
+    const fixture = TestBed.createComponent(MovieRatingComponent);
+    fixture.componentInstance.rating = 'pg';
+
+    const compiled = fixture.nativeElement as HTMLElement;
+    fixture.detectChanges();
+
+    expect(compiled.querySelector('span')).toHaveClass('text-green-500');
+  });
+
+  it('applies the correct text color for rating "pg-13"', () => {
+    const fixture = TestBed.createComponent(MovieRatingComponent);
+    fixture.componentInstance.rating = 'pg-13';
+
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    fixture.detectChanges();
+    expect(compiled.querySelector('span')).toHaveClass('text-yellow-500');
+  });
+
+  it('applies the correct text color for rating "r"', () => {
+    const fixture = TestBed.createComponent(MovieRatingComponent);
+    fixture.componentInstance.rating = 'r';
+
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    fixture.detectChanges();
+    expect(compiled.querySelector('span')).toHaveClass('text-red-500');
+  });
+
+  it('applies the correct text color for rating "nc-17"', () => {
+    const fixture = TestBed.createComponent(MovieRatingComponent);
+    fixture.componentInstance.rating = 'nc-17';
+
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    fixture.detectChanges();
+    expect(compiled.querySelector('span')).toHaveClass('text-red-700');
+  });
+
+  it('applies the default text color for unknown ratings', () => {
+    const fixture = TestBed.createComponent(MovieRatingComponent);
+    fixture.componentInstance.rating = 'unknown';
+
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    fixture.detectChanges();
+    expect(compiled.querySelector('span')).toHaveClass('text-white');
   });
 });
