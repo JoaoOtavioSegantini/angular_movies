4dc98244b8694508143cd0b65c59b9c6e12acaa5
João Otávio <joaotavioos@outlook.com>
2024-01-03 10:11:03 -0300

    feat: add register route ✨

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -3,13 +3,27 @@ import { SearchComponent } from './page/search/search.component';
 import { LayoutComponent } from './layout/layout.component';
 import { WatchComponent } from './page/watch/watch.component';
 import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
   },
-  { path: 'search', component: SearchComponent },
-  { path: 'watch/:id', component: WatchComponent },
-  { path: 'auth/login', component: LoginComponent },
+  {
+    path: 'search',
+    component: SearchComponent,
+  },
+  {
+    path: 'watch/:id',
+    component: WatchComponent,
+  },
+  {
+    path: 'auth/login',
+    component: LoginComponent,
+  },
+  {
+    path: 'auth/register',
+    component: RegisterComponent,
+  },
 ];
